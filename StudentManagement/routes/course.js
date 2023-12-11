95ac8a4b9dc97311d0dac18256745d66a29de55c
suppra4347 <suppra4347@gmail.com>
2023-12-11 11:50:16 +0530

    .

--- StudentManagement/routes/course.js
+++ StudentManagement/routes/course.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
+router.use(express.json())
 
 //course end points
 
@@ -20,17 +21,36 @@ router.get('/course/:id', (req, res) => {
 router.post('/course', (req, res) => {
     const courses = JSON.parse(fs.readFileSync('./course.json'));
     const nextID = courses[courses.length-1].id+1;
-    console.log(nextID);
-    courses.push({"id":nextID, "name":req.body.name})
+    console.log(nextID + ", " + req.body.name);
+    courses.push({id:nextID, name:req.body.name});
+    fs.writeFileSync('./course.json', JSON.stringify(courses));
     res.send("course data inserted");
 });
 
 router.put('/course', (req, res) => {
-    res.send("course data updated");
+    const courses = JSON.parse(fs.readFileSync('./course.json'));
+    const id = parseInt(req.body.id);
+    const newName = req.body.name;
+    let flag = 0;
+    for(let i = 0; i < courses.length; i++){
+        if(courses[i].id == id) {
+            courses[i].name = newName;
+            flag = 1;
+            break;
+        }
+    }
+    if(flag == 1) {
+        fs.writeFileSync('./course.json', JSON.stringify(courses));
+        res.send("Course updated");
+    }
+    res.send("No Course found");
 });
 
 router.delete('/course', (req, res) => {
-    res.send("course data deleted");
+    const courses = JSON.parse(fs.readFileSync('./course.json'));
+    const id = parseInt(req.body.id);
+    
+    // res.send("course data deleted");
 });
 
 module.exports = router;
